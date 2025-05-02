ea39cabaf9512de5d6d06361b20e8142cc456d46
Joonsu Yoon <bananamilk452@gmail.com>
2025-05-02 20:21:21 +0900

    queueFactory 개선

--- utils/queueFactory.ts
+++ utils/queueFactory.ts
@@ -9,10 +9,49 @@ export interface QueueHandler<Input> {
   process: (job: Job<Input>) => Promise<unknown>;
 }
 
+// 활성 큐들을 저장할 맵
+const activeQueues = new Map<string, Queue>();
+const activeWorkers = new Map<string, Worker>();
+
+// 애플리케이션 종료 시 모든 큐와 워커를 정리
+function cleanupQueuesAndWorkers() {
+  console.log(
+    `Cleaning up queues and workers... Queues: ${activeQueues.size}, Workers: ${activeWorkers.size}`,
+  );
+  for (const [, queue] of activeQueues) {
+    queue.close();
+  }
+  for (const [, worker] of activeWorkers) {
+    worker.removeAllListeners();
+    worker.close();
+  }
+  activeQueues.clear();
+  activeWorkers.clear();
+}
+
+// Node 프로세스가 종료될 때 정리
+["SIGINT", "SIGTERM", "SIGQUIT"].forEach((signal) => {
+  process.on(signal, () => {
+    cleanupQueuesAndWorkers();
+    process.exit(0);
+  });
+});
+
 export function createQueue<Input>(
   queueName: string,
   processHandler: (job: Job<Input>) => Promise<unknown>,
 ): QueueHandler<Input> {
+  if (activeQueues.has(queueName)) {
+    const existingQueue = activeQueues.get(queueName)!;
+
+    return {
+      addJob: async (data: Input, options?: JobsOptions) => {
+        return existingQueue.add(queueName, data, options);
+      },
+      process: processHandler,
+    };
+  }
+
   const connection = {
     host: process.env.VALKEY_HOST,
     port: Number(process.env.VALKEY_PORT),
@@ -40,15 +79,18 @@ export function createQueue<Input>(
       returnvalue = JSON.stringify(job.returnvalue, null, 2);
     }
     logger.info(
-      `Job ${job.name}#${job.id} completed with result ${returnvalue}`,
+      `Job ${job.name}#${job.id}이 완료되었습니다. 값: ${returnvalue}`,
     );
   });
 
   worker.on("failed", (job, err) => {
     logger.error(
-      `Job ${job?.name}#${job?.id} failed with error ${err.message}`,
+      `Job ${job?.name}#${job?.id}이 실패했습니다. 에러: ${err.message}`,
     );
   });
+
+  activeQueues.set(queueName, queue);
+  activeWorkers.set(queueName, worker);
 
   return {
     addJob: async (data: Input, options?: JobsOptions) => {
