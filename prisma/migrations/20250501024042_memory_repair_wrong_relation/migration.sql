b766be105907f8791b8b730e69c1d9134293055f
Joonsu Yoon <bananamilk452@gmail.com>
2025-05-02 20:20:29 +0900

    프리즈마 스키마에 메모리 모델 추가

---
+++ prisma/migrations/20250501024042_memory_repair_wrong_relation/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `embeddingId` on the `memory` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "memory" DROP COLUMN "embeddingId";
