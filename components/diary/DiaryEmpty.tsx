bc916d7d68ef5a1d014e604160cf6e9faee75cea
Joonsu Yoon <bananamilk452@gmail.com>
2025-05-03 16:44:20 +0900

    오늘 일기 쓰기 버튼으로 new로 가면 date 넘겨주기

--- components/diary/DiaryEmpty.tsx
+++ components/diary/DiaryEmpty.tsx
@@ -1,14 +1,21 @@
+import { format } from "date-fns";
 import Link from "next/link";
 
 import { Button } from "~/components/ui/button";
 
-export function DiaryEmpty() {
+interface DiaryEmptyProps {
+  date?: Date;
+}
+
+export function DiaryEmpty({ date }: DiaryEmptyProps) {
+  const url = date ? `/new?date=${format(date, "yyyy-MM-dd")}` : "/new";
+
   return (
     <div className="flex flex-col gap-4 p-6 justify-center items-center bg-gray-200 rounded-lg border border-dashed border-gray-500">
       <h2 className="text-lg">
         아직 작성된 일기가 없어요. 오늘의 일기를 작성해보세요!
       </h2>
-      <Link href="/new" className="hover:underline">
+      <Link href={url} className="hover:underline">
         <Button>일기 쓰러 가기</Button>
       </Link>
     </div>
