52aff2280c720617eaf0042b84d283cefa419053
Joonsu Yoon <bananamilk452@gmail.com>
2025-05-12 22:29:15 +0900

    default layout 추가

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,17 +1,9 @@
-import { AppSidebar } from "~/components/AppSidebar";
-import { SidebarProvider } from "~/components/ui/sidebar";
+import DefaultLayout from "~/layouts/default";
 
 export default function HomeLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <div className="w-full sm:w-3/4 md:w-3/5 xl:w-1/2 mx-auto">
-        {children}
-      </div>
-    </SidebarProvider>
-  );
+  return <DefaultLayout>{children}</DefaultLayout>;
 }
