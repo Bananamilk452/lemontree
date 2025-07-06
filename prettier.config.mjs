8bcfc4b575ea458a9480481448324552639518c9
Joonsu Yoon <bananamilk452@gmail.com>
2025-07-06 19:09:16 +0900

    prettier 설정 버그 수정 (tailwind가 제일 마지막에 와야함)

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: [
-    "prettier-plugin-tailwindcss",
     "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
   ],
   importOrder: [
     "<THIRD_PARTY_MODULES>",
