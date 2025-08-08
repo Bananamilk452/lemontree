f0a174d7b8faf3577ff1bee5f72215f09565d91f
Joonsu Yoon <bananamilk452@gmail.com>
2025-08-08 12:49:55 +0900

    홈 빌드 시 날짜로 고정되는 버그 수정

--- components/diary/DiaryViewer.tsx
+++ components/diary/DiaryViewer.tsx
@@ -13,7 +13,7 @@ import { Spinner } from "~/components/Spinner";
 import type { Diary } from "~/lib/models/diary";
 
 interface DiaryViewerProps {
-  diary: Diary | null;
+  diary?: Diary | null;
 }
 
 export function DiaryViewer({ diary: initialDiary }: DiaryViewerProps) {
