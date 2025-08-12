c0af636762f6205daa7b5c2b0cc4b99046712933
Joonsu Yoon <bananamilk452@gmail.com>
2025-08-12 20:04:03 +0900

    메모리에 날짜 클릭하여 해당 일기 볼 수 있게 모달 기능

---
+++ components/diary/DiaryViewModal.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { getDiaryByDate } from "~/app/actions/diary";
+import { DiaryListCard } from "~/components/diary/DiaryListCard";
+import { Spinner } from "~/components/Spinner";
+import { Button } from "~/components/ui/button";
+import { Dialog, DialogContent, DialogFooter } from "~/components/ui/dialog";
+
+interface DiaryViewModalProps {
+  date: string;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+export function DiaryViewModal({ date, open, setOpen }: DiaryViewModalProps) {
+  const { data, status } = useQuery({
+    queryKey: ["diary", date],
+    queryFn: () => getDiaryByDate(new Date(date)),
+  });
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="max-h-[calc(100dvh-24px)] max-w-[calc(100dvw-24px)] !overflow-auto sm:max-w-3xl">
+        {status === "pending" && (
+          <div className="flex flex-col items-center gap-1 pt-4">
+            <Spinner />
+            <p className="text-sm">일기 로딩 중...</p>
+          </div>
+        )}
+        {status === "success" && data && <DiaryListCard diary={data} />}
+        {status === "success" && !data && (
+          <div>해당 날짜의 일기를 찾을 수 없습니다.</div>
+        )}
+        {status === "error" && (
+          <div>일기를 불러오는 중 오류가 발생했습니다.</div>
+        )}
+
+        <DialogFooter className="flex items-center justify-end gap-2">
+          <Button
+            variant="outline"
+            type="button"
+            onClick={() => {
+              setOpen(false);
+            }}
+          >
+            닫기
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
