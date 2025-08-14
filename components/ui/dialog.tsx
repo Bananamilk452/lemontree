fe68768935d7f93befca92d0d1f7937168f1066e
Joonsu Yoon <bananamilk452@gmail.com>
2025-08-14 14:09:37 +0900

    dialog 컴포넌트에서 close 버튼 제거 및 Footer 정렬 row로 변경

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-import { cn } from "~/utils";
-import { XIcon } from "lucide-react";
 import * as React from "react";
+
+import { cn } from "~/utils";
 
 function Dialog({
   ...props
@@ -62,10 +62,6 @@ function DialogContent({
         {...props}
       >
         {children}
-        <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
-          <XIcon />
-          <span className="sr-only">Close</span>
-        </DialogPrimitive.Close>
       </DialogPrimitive.Content>
     </DialogPortal>
   );
@@ -85,10 +81,7 @@ function DialogFooter({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="dialog-footer"
-      className={cn(
-        "flex flex-col-reverse gap-2 sm:flex-row sm:justify-end",
-        className,
-      )}
+      className={cn("flex flex-row gap-2 sm:justify-end", className)}
       {...props}
     />
   );
