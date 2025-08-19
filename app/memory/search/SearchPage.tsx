2421aabe25e13eb8f39fe2abbe655f2e72cd11cf
Joonsu Yoon <bananamilk452@gmail.com>
2025-08-19 12:28:54 +0900

    Searchbar만 있는 페이지 (검색 랜딩 페이지) 추가

---
+++ app/memory/search/SearchPage.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { SearchBar } from "~/components/search/SearchBar";
+
+interface SearchParams {
+  searchInput: string;
+  semanticSearch: boolean;
+  sort: string;
+}
+
+export function SearchPage() {
+  const router = useRouter();
+
+  function onSearch({ searchInput, semanticSearch, sort }: SearchParams) {
+    const searchParams = new URLSearchParams();
+    searchParams.set("q", searchInput);
+    searchParams.set("semantic", String(semanticSearch));
+    searchParams.set("sort", sort);
+
+    router.push(`/memory/search/1?${searchParams.toString()}`);
+  }
+
+  return (
+    <>
+      <SearchBar onSearch={onSearch} />
+
+      <hr className="my-6" />
+    </>
+  );
+}
