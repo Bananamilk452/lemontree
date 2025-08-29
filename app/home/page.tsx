c36ec9acd6f99e94381b06b67e1985d19310fff4
Joonsu Yoon <bananamilk452@gmail.com>
2025-08-29 16:22:21 +0900

    /home에 SentimentChart SSR 되게 추가

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,16 +1,35 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+
 import { DiaryViewer } from "~/components/diary/DiaryViewer";
 import { Header } from "~/components/Header";
+import { SentimentChart } from "~/components/home/SentimentChart";
 import { MainContainer } from "~/components/ui/container";
 
-export default function Home() {
+import { getSentimentByDate } from "../actions/diary";
+
+export default async function Home() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["sentiment", 1, 7],
+    queryFn: () => getSentimentByDate({ page: 1, limit: 7 }),
+  });
+
   return (
     <>
       <Header>
         <h1 className="text-2xl font-bold">홈</h1>
       </Header>
 
-      <MainContainer className="flex flex-col gap-4">
+      <MainContainer className="flex flex-col gap-6">
         <DiaryViewer />
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <SentimentChart />
+        </HydrationBoundary>
       </MainContainer>
     </>
   );
