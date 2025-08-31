e0c6ab6a0afe5b86a1ad8822c5751f1692b363c7
Joonsu Yoon <bananamilk452@gmail.com>
2025-08-31 12:50:40 +0900

    better-auth admin 플러그인 추가 및 prisma migrate

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,9 @@
+import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: process.env.BASE_URL,
+  plugins: [adminClient()],
 });
 
 export type Session = typeof authClient.$Infer.Session;
