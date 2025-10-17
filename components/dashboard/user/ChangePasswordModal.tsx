dac66c193df87a43a6e0e8eb55829cb6f75e6a08
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-17 18:13:16 +0900

    dashboard ChangePasswordModal 폼 비밀번호 확인 입력 안할 시 문구 수정

--- components/dashboard/user/ChangePasswordModal.tsx
+++ components/dashboard/user/ChangePasswordModal.tsx
@@ -38,7 +38,7 @@ interface ChangePasswordModalProps {
 const changePasswordFormSchema = z
   .object({
     password: z.string().min(6, "비밀번호는 최소 6자 이상이어야 합니다."),
-    passwordConfirm: z.string(),
+    passwordConfirm: z.string().min(1, "비밀번호 확인을 입력해주세요."),
   })
   .refine((data) => data.password === data.passwordConfirm, {
     message: "비밀번호가 일치하지 않습니다.",
